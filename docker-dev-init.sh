c25b398a01dade1fc76e08202ef304a0c0c7413d
Your Name <koom@c1>
2025-02-13 18:12:17 +0100

    lock..

--- docker-dev-init.sh
+++ docker-dev-init.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-npm i
-
-#rm -rf ./node_modules/yellow-server-common || true
-#ln -s ../../yellow-server-common ./node_modules/
+bun i --frozen-lockfile
 
--- start-dev.sh
+++ start-dev.sh
@@ -2,4 +2,5 @@
 
 echo -ne "\033]0;YELLOW ADMIN\007"
 #bun --bun run dev
+bun i --frozen-lockfile
 npm run dev
