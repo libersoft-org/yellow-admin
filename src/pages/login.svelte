90df7f3bba81c0e76752b983f9eebfad6b42ac72
LiberSoft <info@libersoft.org>
2025-02-17 16:46:56 +0100

    Rename vite variables

--- src/pages/login.svelte
+++ src/pages/login.svelte
@@ -14,9 +14,9 @@
   credentials.server = (window.location.protocol === 'https:' ? 'wss://' : 'ws://') + window.location.host + '/';
   if (import.meta.env.VITE_SERVER) {
    credentials = {
-    server: import.meta.env.VITE_SERVER,
-    username: import.meta.env.VITE_USERNAME,
-    password: import.meta.env.VITE_PASSWORD
+    server: import.meta.env.VITE_ADMIN_SERVER,
+    username: import.meta.env.VITE_ADMIN_USERNAME,
+    password: import.meta.env.VITE_ADMIN_PASSWORD
    };
    clickLogin();
   }
